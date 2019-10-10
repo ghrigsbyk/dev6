948b8db120e8812c09aa54657093ff396c3a121a
ghrigsbyk <48164580+ghrigsbyk@users.noreply.github.com>
2019-10-10 18:58:12 -0500

    Delete script.js

--- dev6/js/script.js
+++
@@ -1,8 +0,0 @@
-var person = window.prompt("Please enter your name", );
-
-if (person != null) {
-  window.alert("Hello " + person + "! How are you today?");
-  
-}
-
-console.log(window.alert("Hello " + person + "! How are you today?"));
