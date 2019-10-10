d05f9fc02a9010ebc6855af5d4cfa1bfd66c3e53
ghrigsbyk <48164580+ghrigsbyk@users.noreply.github.com>
2019-10-10 18:59:41 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var person = window.prompt("Please enter your name", );
+
+if (person != null) {
+  window.alert("Hello " + person + "! How are you today?");
+  
+}
+
+console.log(window.alert("Hello " + person + "! How are you today?"));
